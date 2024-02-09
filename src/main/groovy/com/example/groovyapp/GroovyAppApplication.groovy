e0b88509b5dd19dc29601c53da76c0bbb9184b98
yonasabraha <84365310+yonasabraha@users.noreply.github.com>
2024-02-09 12:15:47 -0800

    intial push

---
+++ src/main/groovy/com/example/groovyapp/GroovyAppApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.groovyapp
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class GroovyAppApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(GroovyAppApplication, args)
+    }
+
+}
